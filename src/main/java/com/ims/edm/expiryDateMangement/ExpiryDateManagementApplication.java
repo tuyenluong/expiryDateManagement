da121a40603bc4c1136b2e70a4fbbb8b3507c369
tuyen.luong <atuyenluong@gmail.com>
2025-04-17 21:25:37 +0700

    first commit

---
+++ src/main/java/com/ims/edm/expiryDateMangement/ExpiryDateManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ims.edm.expiryDateMangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpiryDateManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpiryDateManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ims/edm/expiryDateMangement/ExpiryDateMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ims.edm.expiryDateMangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpiryDateMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
