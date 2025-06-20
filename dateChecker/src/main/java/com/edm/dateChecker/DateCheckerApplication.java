e9859baf85bbea3e08b94bfb11b095f7d5a5f4f0
tuyen.luong <atuyenluong@gmail.com>
2025-06-20 13:33:08 +0700

    add new module date checker

---
+++ dateChecker/src/main/java/com/edm/dateChecker/DateCheckerApplication.java
@@ -0,0 +1,12 @@
+package com.edm.dateChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DateCheckerApplication.class, args);
+    }
+}
